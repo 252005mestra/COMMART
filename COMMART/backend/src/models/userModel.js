b7e9ee404f0d444eb565904c69ed57d579edb3a5
Isabela Ochoa <ibela200618@gmail.com>
2025-04-21 23:10:21 -0500

    Agrega logica de back de registro y mejora estructuración de carpetas

---
+++ COMMART/backend/src/models/userModel.js
@@ -0,0 +1,31 @@
+import connection from '../config/db.js';
+
+// Registro de Usuario
+export const createUser = (username, email, hashedPassword) => {
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, hashedPassword], (err, result) => { 
+        if (err) return reject(err); 
+        resolve(result); 
+    });
+  })
+};
+
+// Encontrar el usuario por correo electrónico
+export const findUserByEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+      if (err) return reject(err);
+      resolve(results[0]); 
+    });
+  });
+};
+
+// Encontrar el usuario por nombre de usuario
+export const findUserByUsername = (username) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM users WHERE username = ?', [username], (err, results) => {
+      if (err) return reject(err);
+      resolve(results[0]);
+    });
+  });
+};
